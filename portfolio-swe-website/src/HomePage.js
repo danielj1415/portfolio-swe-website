4b0f4ea3bc9d005c28650c6cfbb0443e3c1860a7
Daniel Jong <danielj1415@gmail.com>
2024-05-05 16:43:39 -0700

    added fonts and some pages

---
+++ portfolio-swe-website/src/HomePage.js
@@ -0,0 +1,11 @@
+import './HomePage.css';
+
+function HomePage(){
+    return(
+        <div>
+            <p className = "headerText">This is the Home Page</p>
+        </div>
+    )
+}
+
+export default HomePage
