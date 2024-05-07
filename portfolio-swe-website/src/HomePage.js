7bb4b591cb112f8c418918fd0b4ce49d86329c82
Daniel Jong <danielj1415@gmail.com>
2024-05-06 18:47:21 -0700

    coded the navigation menu and hero section

--- portfolio-swe-website/src/HomePage.js
+++ portfolio-swe-website/src/HomePage.js
@@ -3,7 +3,20 @@ import './HomePage.css';
 function HomePage(){
     return(
         <div>
-            <p className = "headerText">This is the Home Page</p>
+            <div className = "mainContainer">
+                <div className = "navigationBar">
+                    <p className = "navigationBarText">Daniel Jong</p>
+                    <div className = "navigationBarRightItems">
+                        <p className = "navigationBarText">Work</p>
+                        <p className = "navigationBarText navigationBarAbout">About</p>
+                        <p className = "navigationBarText">Contact</p>
+                    </div>
+                </div>
+                <p className = "heroSectionHeadingText">Hello, I'm Daniel!</p>
+                <div className = "heroSectionSubheadingContainer">
+                    <p className = "heroSectionSubheadingText">Software engineer passionate about turning ideas into designs and designs into code.</p>
+                </div>
+            </div>
         </div>
     )
 }
