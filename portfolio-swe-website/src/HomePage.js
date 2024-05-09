a45575850909721bea9fe00a882d8ae7db498e64
Daniel Jong <danielj1415@gmail.com>
2024-05-09 10:38:11 -0700

    added basic structure for project section, only two projects though

--- portfolio-swe-website/src/HomePage.js
+++ portfolio-swe-website/src/HomePage.js
@@ -19,19 +19,57 @@ function HomePage(){
                     </div>
                 </div>
                 <div className = "projectsSection">
-                    <p>Plant Hydroponics App</p>
-                    <div>
-                        <p>UI Design</p>
-                    </div>
-                    <div>
-                        <p>Mobile Development</p>
-                    </div>
-                    <div>
-                        <p>Application that enables users to change the pH of water for their hydroponics system</p>
-                    </div>
-                    <div className = "image">
+                    {/*Project: Plant Hydroponics App*/}
+                    <div className = "projectContainer">
+                        <div className = "projectText">
+                            <p className = "projectName">Plant Hydroponics App</p>
+                            <div className = "projectTagContainer">
+                                <div className = "projectTagPlant">
+                                    <p className = "projectTagText">UI Design</p>
+                                </div>
+                                <div className = "projectTagPlant">
+                                    <p className = "projectTagText">Mobile Development</p>
+                                </div>
+                            </div>
+                            <div className = "projectDescriptionContainer">
+                                <p className = "projectDescriptionText">Application that enables users to change the pH of water for their hydroponics system.</p>
+                            </div>
+                        </div>
+                        <div className = "projectImagePlant">
 
+                        </div>
                     </div>
+                    {/*Project: Spotify Clone Website*/}
+                    <div className = "projectContainer">
+                        <div className = "projectText">
+                            <p className = "projectName">Spotify Clone Website</p>
+                            <div className = "projectTagContainer">
+                                <div className = "projectTagSpotify">
+                                    <p className = "projectTagText">Web Development</p>
+                                </div>
+                            </div>
+                            <div className = "projectDescriptionContainer">
+                                <p className = "projectDescriptionText">Website that aims to replicate the functionality and interface of the Spotify Web Player. </p>
+                            </div>
+                        </div>
+                        <div className = "projectImageSpotify">
+
+                        </div>
+                    </div>
+                </div>
+                <div className = "footerSection">
+                    <div className = "footerLargeContainer">
+                        <div className = "footerTextContainer">
+                            <p className = "footerText">Turning ideas into designs and designs into code.</p>
+                        </div>
+                        <p className = "footerEmail">email: danielj1415@gmail.com</p>
+                        <p className = "footerPhone">phone: (626) 518-2422</p>
+                        <div className = "iconContainer">
+                            <img className = "imageIcon"/>
+                            <img className = "imageIcon"/>
+                        </div>
+                    </div>
+                    <p className = "footerText2">Made with 🧋☕️✨ </p>
                 </div>
             </div>
         </div>
